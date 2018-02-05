96bc3d7dd0352ee78e83dbfc8aca73cf9bcff216
DanielmColey <dmcoley948562@gmail.com>
2018-02-05 12:12:34 -0700

    add contact page with map with styles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(40.5725193, -111.8985836),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
